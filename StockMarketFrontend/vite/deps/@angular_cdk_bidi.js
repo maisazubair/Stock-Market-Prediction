b939d45850d4f4bbfc99a6e35d4d691e9971e01a
maisazubair <zubairmaisa@gmail.com>
2025-10-23 11:50:25 +0500

    Initial commit - Stock Market Prediction System

---
+++ StockMarketFrontend/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,15 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-HJ7VCZ3E.js";
+import "./chunk-FHJV52JW.js";
+import "./chunk-JUJSAYHL.js";
+import "./chunk-Z56SSN6E.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ StockMarketFrontend/vite/deps/chunk-VUZWPMB5.js
@@ -0,0 +1,38 @@
+import {
+  NgModule,
+  setClassMetadata,
+  ɵɵdefineInjector,
+  ɵɵdefineNgModule
+} from "./chunk-FHJV52JW.js";
+import {
+  __name,
+  __publicField
+} from "./chunk-Z56SSN6E.js";
+
+// node_modules/@angular/cdk/fesm2022/scrolling2.mjs
+var RtlScrollAxisType;
+(function(RtlScrollAxisType2) {
+  RtlScrollAxisType2[RtlScrollAxisType2["NORMAL"] = 0] = "NORMAL";
+  RtlScrollAxisType2[RtlScrollAxisType2["NEGATED"] = 1] = "NEGATED";
+  RtlScrollAxisType2[RtlScrollAxisType2["INVERTED"] = 2] = "INVERTED";
+})(RtlScrollAxisType || (RtlScrollAxisType = {}));
+
+// node_modules/@angular/cdk/fesm2022/platform.mjs
+var _PlatformModule = class _PlatformModule {
+};
+__name(_PlatformModule, "PlatformModule");
+__publicField(_PlatformModule, "ɵfac", /* @__PURE__ */ __name(function PlatformModule_Factory(__ngFactoryType__) {
+  return new (__ngFactoryType__ || _PlatformModule)();
+}, "PlatformModule_Factory"));
+__publicField(_PlatformModule, "ɵmod", ɵɵdefineNgModule({
+  type: _PlatformModule
+}));
+__publicField(_PlatformModule, "ɵinj", ɵɵdefineInjector({}));
+var PlatformModule = _PlatformModule;
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(PlatformModule, [{
+    type: NgModule,
+    args: [{}]
+  }], null, null);
+})();
+//# sourceMappingURL=chunk-VUZWPMB5.js.map
